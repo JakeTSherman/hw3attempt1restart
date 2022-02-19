5dd391700cb5038d81438e203cc14361f484d954
JakeTSherman <jake.sherman@kellogg.northwestern.edu>
2022-02-19 23:19:12 +0000

    getting started

--- app/controllers/place_controller.rb
+++ app/controllers/place_controller.rb
@@ -1,6 +1,19 @@
 class PlaceController < ApplicationController
+   
+    def index
+        @places = Place.all
+      end
 
-    def index 
-        @places = Place.all 
-    end
+    
+      def new 
+        @place = Place.new
+      end
+    
+      def create
+        #values = params["company"]
+        @place = Place.new(params["places"])
+        @place.save
+        redirect_to "/home"
+      end
+
 end
